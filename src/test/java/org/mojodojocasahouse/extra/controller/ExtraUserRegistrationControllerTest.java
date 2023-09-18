00ac6f9d7698b18e95fcf4cbc5fec3bc9f093509
Pedro Ortiz de Guinea <66389082+peportizdg@users.noreply.github.com>
2023-09-18 15:17:33 -0300

    sacados tags innecesarios o sin uso

--- src/test/java/org/mojodojocasahouse/extra/controller/ExtraUserRegistrationControllerTest.java
+++ src/test/java/org/mojodojocasahouse/extra/controller/ExtraUserRegistrationControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
