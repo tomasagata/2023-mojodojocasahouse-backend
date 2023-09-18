3aee37f58bac8acaf7917132299eeac2497916c6
Pedro Ortiz de Guinea <66389082+peportizdg@users.noreply.github.com>
2023-09-18 15:17:02 -0300

    adiciones para el login

--- src/main/java/org/mojodojocasahouse/extra/controller/ExtraUserRegistrationController.java
+++ src/main/java/org/mojodojocasahouse/extra/controller/ExtraUserRegistrationController.java
@@ -3,7 +3,6 @@ package org.mojodojocasahouse.extra.controller;
 import jakarta.validation.Valid;
 import org.mojodojocasahouse.extra.dto.ExtraUserRegistrationResponseDto;
 import org.mojodojocasahouse.extra.service.ExtraUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,21 +14,9 @@ import org.mojodojocasahouse.extra.dto.ExtraUserRegistrationDto;
 public class ExtraUserRegistrationController {
 
     private final ExtraUserService userService;
-
-    @Autowired
     public ExtraUserRegistrationController(ExtraUserService userService) {
         this.userService = userService;
     }
-
-//    @ModelAttribute("usuario")
-//    public UsuarioRegistroDTO usuarioRegistroDTO() {
-//        return new UsuarioRegistroDTO();
-//    }
-
-//    @GetMapping("/registro")
-//    public String mostrarFormularioRegistro() {
-//        return "registroTemplate";
-//    }
 
     @PostMapping(value = "/register", consumes = "application/json", produces = "application/json")
     public ResponseEntity<ExtraUserRegistrationResponseDto> registerUserAccount(@Valid @RequestBody ExtraUserRegistrationDto extraUserRegistrationDto) {
--- src/main/java/org/mojodojocasahouse/extra/service/impl/ExtraUserServiceImpl.java
+++ src/main/java/org/mojodojocasahouse/extra/service/impl/ExtraUserServiceImpl.java
@@ -6,7 +6,6 @@ import org.mojodojocasahouse.extra.dto.ExtraUserRegistrationResponseDto;
 import org.mojodojocasahouse.extra.model.impl.ExtraUser;
 import org.mojodojocasahouse.extra.repository.ExtraUserRepository;
 import org.mojodojocasahouse.extra.service.ExtraUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +13,6 @@ public class ExtraUserServiceImpl implements ExtraUserService {
 
     private final ExtraUserRepository userRepository;
 
-    @Autowired
     public ExtraUserServiceImpl(ExtraUserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -22,8 +20,7 @@ public class ExtraUserServiceImpl implements ExtraUserService {
     public ExtraUserRegistrationResponseDto registrarUsuario(ExtraUserRegistrationDto userRegistrationDto)
                                                             throws ConstraintViolationException {
         ExtraUser usuario = new ExtraUser(userRegistrationDto.getFirstName(), userRegistrationDto.getLastName(), userRegistrationDto.getEmail(), userRegistrationDto.getPassword());
-        ExtraUser registeredUser = userRepository.save(usuario);
-        // Do something with registered user
+        userRepository.save(usuario);
         return new ExtraUserRegistrationResponseDto("ExtraUser created successfully");
     }
 }
